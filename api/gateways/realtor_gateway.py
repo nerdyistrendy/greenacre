73aacf44b65b2bcfddef87c672a68a4d3bfed718
Li Dai <li.lea.dai@gmail.com>
2021-02-01 11:01:06 -0800

    dot env

--- api/gateways/realtor_gateway.py
+++ api/gateways/realtor_gateway.py
@@ -1,9 +1,13 @@
 import requests
 import json
-
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 class RealtorGateway:
-    API_KEY = "e6a6cc616cmsh356cb445305c5aap10de3fjsn2152bcb8d556"
+    
+    API_KEY = os.getenv('API_KEY')
+
 
     def show_details(self, property_id) -> str:
         url = "https://realtor.p.rapidapi.com/properties/v2/detail?="
@@ -18,7 +22,6 @@ class RealtorGateway:
         response = requests.request(
             "GET", url, data=payload, headers=headers, params=params)
         return response.text
-
 
 # myclass = RealtorGateway()
 # details = json.loads(myclass.show_details("O3599084026"))
--- api/setting.py
+++ api/setting.py
@@ -1,2 +1,9 @@
-# from os import environ
-# API_KEY = environ.get('API_KEY')
+from os import environ
+from dotenv import load_dotenv
+from pathlib import Path  # Python 3.6+ only
+import os
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+API_KEY = os.getenv('API_KEY')
