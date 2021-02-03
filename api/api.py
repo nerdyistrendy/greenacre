eca9064803229aa5a44badac6e151d90f8688192
Li Dai <li.lea.dai@gmail.com>
2021-02-02 23:11:15 -0800

    before setting up database

--- api/api.py
+++ api/api.py
@@ -1,17 +1,23 @@
 from flask import Flask, request, jsonify, render_template
 from gateways.realtor_gateway import RealtorGateway
 from flask_cors import CORS, cross_origin
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/greenacre"
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 realtor_GW = RealtorGateway()
 cors = CORS(app)
+
+
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
-
 @app.route("/details/<property_id>")
-
 def get_property_details(property_id):
     property_details_json = realtor_GW.show_details(property_id)
 
@@ -22,11 +28,6 @@ def get_property_id(address):
     property_details_json = realtor_GW.get_property_id_by_address(address)
 
     return property_details_json;
-
-
-# @app.route("/mls/<mls_id>")
-# def get_by_mls(mls_id):
-#     return realtor_GW.mls(mls_id)
 
 if __name__ == '__main__':
     app.run(debug=True)
