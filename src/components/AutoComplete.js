40fd9da99fc8c038ce7443a4163a31e6bb944d5a
Li Dai <li.lea.dai@gmail.com>
2021-02-04 20:41:42 -0800

    added form ui

--- src/components/AutoComplete.js
+++ src/components/AutoComplete.js
@@ -1,15 +1,16 @@
-import React, { Component } from 'react'
-import PlacesAutocomplete from 'react-places-autocomplete';
-import Details from './Details';
+import React, { Component } from "react";
+import PlacesAutocomplete from "react-places-autocomplete";
+import Details from "./Details";
+import "./styles.scss";
 
-import './AutoComplete.css'; 
+import "./AutoComplete.css";
 export class AutoComplete extends Component {
   constructor(props) {
     super(props);
-    this.state = { address: '' };
+    this.state = { address: "" };
   }
- 
-  handleChange = address => {
+
+  handleChange = (address) => {
     this.setState({ address });
   };
 
@@ -21,57 +22,60 @@ export class AutoComplete extends Component {
     // this.setState({ address: '' });
   };
 
- 
   render() {
     return (
       <div className="canvas">
-      <PlacesAutocomplete
-      value={this.state.address}
-      onChange={this.handleChange}
-      onSelect={this.handleSelect}
-    >
-      {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-        <form onSubmit={this.onFormSubmit}>
-          <input
-            {...getInputProps({
-              placeholder: 'Enter Address',
-              className: 'location-search-input',
-            })}
-            
-          />
-          <input 
-            type="submit"
-            name="Submit"
-          />
-          <div className="autocomplete-dropdown-container">
-            {loading && <div>Loading...</div>}
-            {suggestions.map((suggestion, i) => {
-              
-              // inline style for demonstration purpose
-              const style = suggestion.active
-                ? { backgroundColor: '#42a5f5', cursor: 'pointer' }
-                : { backgroundColor: '#ffffff', cursor: 'pointer' };
-              return (
-                <div key={i} className="input-suggestion"
-                  {...getSuggestionItemProps(suggestion, {
-                    
-                    style,
-                  })}
-                >
-                <i className="material-icons"> </i> <span>{suggestion.description}</span>
-                </div>
-              );
-            })}
-          </div>
-          {/* <Search currentProperty={this.props.currentProperty} /> */}
-
-        </form>
-      )}
-    </PlacesAutocomplete>
-
+        <PlacesAutocomplete
+          value={this.state.address}
+          onChange={this.handleChange}
+          onSelect={this.handleSelect}
+        >
+          {({
+            getInputProps,
+            suggestions,
+            getSuggestionItemProps,
+            loading,
+          }) => (
+            <form onSubmit={this.onFormSubmit}>
+              <input
+                {...getInputProps({
+                  placeholder: "Enter Address",
+                  className: "location-search-input",
+                })}
+              />
+              <input type="submit" name="Submit" />
+              <div className="autocomplete-dropdown-container">
+                {loading && <div>Loading...</div>}
+                {suggestions.map((suggestion, i) => {
+                  // inline style for demonstration purpose
+                  const style = suggestion.active
+                    ? { backgroundColor: "#42a5f5", cursor: "pointer" }
+                    : { backgroundColor: "#ffffff", cursor: "pointer" };
+                  return (
+                    <div
+                      key={i}
+                      className="input-suggestion"
+                      {...getSuggestionItemProps(suggestion, {
+                        style,
+                      })}
+                    >
+                      <i className="material-icons"> </i>{" "}
+                      <span>{suggestion.description}</span>
+                    </div>
+                  );
+                })}
+              </div>
+              {/* <Search currentProperty={this.props.currentProperty} /> */}
+            </form>
+          )}
+        </PlacesAutocomplete>
+        <Details
+          currentProperty={this.props.currentProperty}
+          address={this.state.address}
+        />
       </div>
     );
-  };
+  }
 }
 
 export default AutoComplete;
--- src/components/Details.js
+++ src/components/Details.js
@@ -1,9 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import AddToListForm from "./AddToListForm"
+import "./styles.scss";
 
 const Details = (props) => {
   return (
-    <div>{props.currentProperty ? props.currentProperty.properties[0].description: ""}</div>
-  )
-}
+    <div>
+      <ul>
+        <li>
+          {props.currentProperty
+            ? props.currentProperty.properties[0].property_id
+            : ""}
+        </li>
+        <li>{props.currentProperty ? props.address : ""}</li>
+        <li>
+          {props.currentProperty
+            ? props.currentProperty.meta.tracking_params.listingPrice
+            : ""}
+        </li>
+        <li>
+          {props.currentProperty
+            ? props.currentProperty.properties[0].description
+            : ""}
+        </li>
+        <li>{props.currentProperty
+            ? <AddToListForm currentProperty={props.currentProperty}/>
+            : ""}</li>  
+      </ul>
+
+    </div>
+  );
+};
 
 export default Details;
