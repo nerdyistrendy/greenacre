a1ef8bb24ecc54bb0d02601af9ef85ae53c25f34
Li Dai <li.lea.dai@gmail.com>
2021-02-09 20:22:45 -0800

    userlists frontend

--- src/components/List.js
+++ src/components/List.js
@@ -1,8 +1,38 @@
 import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import "./List.scss";
+
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+}));
 
 const List = (props) => {
+  const classes = useStyles();
+
   return (
-    <div>list</div>
+    <div>
+
+      <h1> My lists</h1>
+      {JSON.parse(localStorage.getItem('currentListsLocalStorage')).map(function (list, index) {
+      return (
+        <li key={index}>
+          {list}          
+          </li>
+      );
+    })}
+      <div className="fab">
+      <Fab  aria-label="add" >
+        <AddIcon />
+      </Fab>  
+      </div>
+    </div>
   )
 }
 
