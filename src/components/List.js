3f078318e86014373bdbf35c654149c8888075ad
Li Dai <li.lea.dai@gmail.com>
2021-02-09 21:56:20 -0800

    more userlists frontend

--- src/components/List.js
+++ src/components/List.js
@@ -2,6 +2,15 @@ import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 import "./List.scss";
 
 const useStyles = makeStyles((theme) => ({
@@ -11,7 +20,19 @@ const useStyles = makeStyles((theme) => ({
   extendedIcon: {
     marginRight: theme.spacing(1),
   },
+  table: {
+    minWidth: 650,
+  },
 }));
+
+function createData(name, action) {
+  return { name, action };
+}
+
+const rows = JSON.parse(localStorage.getItem('currentListsLocalStorage')).map(function (list, index) {
+  return (createData(list, "actions"))
+})
+
 
 const List = (props) => {
   const classes = useStyles();
@@ -19,14 +40,28 @@ const List = (props) => {
   return (
     <div>
 
-      <h1> My lists</h1>
-      {JSON.parse(localStorage.getItem('currentListsLocalStorage')).map(function (list, index) {
-      return (
-        <li key={index}>
-          {list}          
-          </li>
-      );
-    })}
+      <h1> My Lists</h1>
+      <TableContainer component={Paper} className="table">
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="center"><strong>List Name</strong></TableCell>
+            <TableCell align="center"><strong>Actions</strong></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row" align="center" >
+                {row.name}
+              </TableCell>
+              <TableCell align="center"><EditIcon /><DeleteForeverIcon /></TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+
       <div className="fab">
       <Fab  aria-label="add" >
         <AddIcon />
